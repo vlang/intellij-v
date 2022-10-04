981f365f912b82f863f87bf156f30d177ff7e90b
Makhnev Petr <51853996+i582@users.noreply.github.com>
2022-10-05 03:37:45 +0400

    added highlighting of return (#81)

---
+++ src/main/kotlin/org/vlang/ide/highlight/exitpoint/VlangFunctionExitPointHandler.kt
@@ -0,0 +1,42 @@
+package org.vlang.ide.highlight.exitpoint
+
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.util.Consumer
+import org.vlang.lang.psi.*
+
+class VlangFunctionExitPointHandler(editor: Editor, file: PsiFile, private val target: PsiElement, private val function: VlangTypeOwner) :
+    HighlightUsagesHandlerBase<PsiElement>(editor, file) {
+
+    override fun getTargets() = listOf(target)
+
+    override fun selectTargets(targets: List<PsiElement>, selectionConsumer: Consumer<in MutableList<out PsiElement>?>) {
+        selectionConsumer.consume(targets.toMutableList())
+    }
+
+    override fun computeUsages(targets: List<PsiElement>) {
+        if (function is VlangFunctionOrMethodDeclaration) {
+            val identifier = function.getIdentifier()
+            if (identifier != null) {
+                addOccurrence(identifier)
+            }
+        }
+
+        object : VlangRecursiveVisitor() {
+            override fun visitFunctionLit(literal: VlangFunctionLit) {}
+            override fun visitReturnStatement(statement: VlangReturnStatement) {
+                addOccurrence(statement)
+            }
+        }.visitTypeOwner(function)
+    }
+
+    companion object {
+        fun createForElement(editor: Editor, file: PsiFile, element: PsiElement): VlangFunctionExitPointHandler? {
+            val function = PsiTreeUtil.getParentOfType(element, VlangFunctionLit::class.java, VlangFunctionOrMethodDeclaration::class.java)
+            return function?.let { VlangFunctionExitPointHandler(editor, file, element, it) }
+        }
+    }
+}
---
+++ src/main/kotlin/org/vlang/ide/highlight/exitpoint/VlangHighlightExitPointsHandlerFactory.kt
@@ -0,0 +1,23 @@
+package org.vlang.ide.highlight.exitpoint
+
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerBase
+import com.intellij.codeInsight.highlighting.HighlightUsagesHandlerFactoryBase
+import com.intellij.openapi.editor.Editor
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFile
+import com.intellij.psi.impl.source.tree.LeafPsiElement
+import org.vlang.ide.highlight.exitpoint.VlangFunctionExitPointHandler.Companion.createForElement
+import org.vlang.lang.VlangTypes
+
+class VlangHighlightExitPointsHandlerFactory : HighlightUsagesHandlerFactoryBase() {
+    override fun createHighlightUsagesHandler(editor: Editor, file: PsiFile, target: PsiElement): HighlightUsagesHandlerBase<*>? {
+        if (target !is LeafPsiElement) return null
+
+        val elementType = target.elementType
+        if (elementType === VlangTypes.RETURN || elementType === VlangTypes.FN) {
+            return createForElement(editor, file, target)
+        }
+
+        return null
+    }
+}
