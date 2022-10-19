4de1ac608bc17772ec93b1f1951706e259ecb7e4
i582 <51853996+i582@users.noreply.github.com>
2022-10-19 20:41:16 +0400

    moved regex for snake case a quick fix to a companion object

--- src/main/kotlin/org/vlang/ide/inspections/namingConventions/VlangNamingConventionInspectionBase.kt
+++ src/main/kotlin/org/vlang/ide/inspections/namingConventions/VlangNamingConventionInspectionBase.kt
@@ -84,7 +84,7 @@ abstract class VlangNamingConventionInspectionBase : VlangBaseInspection() {
             val named = descriptor.psiElement.parentOfType<VlangNamedElement>()
 
             val name = named?.name ?: return
-            val newName = name.replace(Regex("([A-Z])"), "_$1").lowercase().trim('_')
+            val newName = name.replace(SNAKE_CASE_HELPER_REGEX, "_$1").lowercase().trim('_')
 
             invokeLater {
                 RefactoringFactory.getInstance(project).createRename(named, newName).run()
@@ -93,6 +93,7 @@ abstract class VlangNamingConventionInspectionBase : VlangBaseInspection() {
     }
 
     companion object {
+        private val SNAKE_CASE_HELPER_REGEX = Regex("([A-Z])")
         private val NAME_FIX = NameQuickFix()
         private val TO_SNAKE_CASE_FIX = ToSnakeCaseQuickFix()
     }
