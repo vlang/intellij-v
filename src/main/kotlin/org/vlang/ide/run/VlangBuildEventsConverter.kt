b8def9bd42f8755d53d7a9746983b85ea9325c2b
i582 <51853996+i582@users.noreply.github.com>
2022-12-21 21:26:33 +0400

    improved a bit handling of C gen error

--- src/main/kotlin/org/vlang/ide/run/VlangBuildEventsConverter.kt
+++ src/main/kotlin/org/vlang/ide/run/VlangBuildEventsConverter.kt
@@ -15,14 +15,26 @@ import java.util.function.Consumer
 
 class VlangBuildEventsConverter(private val ctx: VlangBuildContext) : BuildOutputParser {
     private val decoder: AnsiEscapeDecoder = AnsiEscapeDecoder()
+    private var cgenError = false
 
     override fun parse(
         line: String,
         reader: BuildOutputInstantReader,
         messageConsumer: Consumer<in BuildEvent>,
     ): Boolean {
+        if (cgenError) {
+            messageConsumer.acceptText(ctx.parentId, line.withNewLine())
+            return false
+        }
+
         val cleanLine = decoder.removeEscapeSequences(line)
         val kind = getMessageKind(cleanLine)
+
+        if (cleanLine.startsWith("=====")) {
+            val event = handleCompilerCgenError()
+            messageConsumer.accept(event)
+            cgenError = true
+        }
 
         if (kind != MessageEvent.Kind.SIMPLE) {
             val event = handleCompilerMessage(reader, kind, cleanLine, line)
@@ -33,12 +45,11 @@ class VlangBuildEventsConverter(private val ctx: VlangBuildContext) : BuildOutpu
         return true
     }
 
-    private fun getMessageKind(line: String): MessageEvent.Kind =
-        when {
-            line.contains("error")   -> MessageEvent.Kind.ERROR
-            line.contains("warning") -> MessageEvent.Kind.WARNING
-            else                     -> MessageEvent.Kind.SIMPLE
-        }
+    private fun getMessageKind(line: String): MessageEvent.Kind = when {
+        line.contains("error")   -> MessageEvent.Kind.ERROR
+        line.contains("warning") -> MessageEvent.Kind.WARNING
+        else                     -> MessageEvent.Kind.SIMPLE
+    }
 
     private fun handleCompilerMessage(
         reader: BuildOutputInstantReader,
@@ -100,6 +111,15 @@ class VlangBuildEventsConverter(private val ctx: VlangBuildContext) : BuildOutpu
         return line + "\n" + allMessage
     }
 
+    private fun handleCompilerCgenError(): FileMessageEventImpl {
+        ctx.errors.incrementAndGet()
+        return FileMessageEventImpl(
+            ctx.parentId,
+            MessageEvent.Kind.ERROR,
+            VLANG_MESSAGE_GROUP, "C gen error", "Error while compiling generated C code. See full build log for details", FilePosition(ctx.workingDirectory.toFile(), 0, 0)
+        )
+    }
+
     private fun getFilePosition(message: String): FilePosition? {
         val parts = message.split(':')
         if (parts.size < 3) {
@@ -123,8 +143,8 @@ class VlangBuildEventsConverter(private val ctx: VlangBuildContext) : BuildOutpu
     companion object {
         const val VLANG_MESSAGE_GROUP: String = "V compiler"
 
-        private val MESSAGE_LINE = Regex(".*\\d+.*\\|.*")
-        private val MESSAGE_ERROR_LINE = Regex(".*\\|.*?~")
+        private val MESSAGE_LINE = Regex(".*\\d+.*\\|.*") //  10 | println('hello world')
+        private val MESSAGE_ERROR_LINE = Regex(".*\\|.*?~") //   | ~~~~~~~
 
         private fun Consumer<in BuildEvent>.acceptText(parentId: Any?, text: String) =
             accept(OutputBuildEventImpl(parentId, text, true))
