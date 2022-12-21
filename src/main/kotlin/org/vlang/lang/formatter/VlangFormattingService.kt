36bdfb588f7889133421e7a0a4364c28b00ba8c6
i582 <51853996+i582@users.noreply.github.com>
2022-12-21 19:21:15 +0400

    improved notification if vfmt found errors

--- src/main/kotlin/org/vlang/lang/formatter/VlangFormattingService.kt
+++ src/main/kotlin/org/vlang/lang/formatter/VlangFormattingService.kt
@@ -1,5 +1,6 @@
 package org.vlang.lang.formatter
 
+import com.intellij.codeEditor.printing.HTMLTextPainter
 import com.intellij.execution.configurations.GeneralCommandLine
 import com.intellij.execution.process.CapturingProcessAdapter
 import com.intellij.execution.process.OSProcessHandler
@@ -7,7 +8,12 @@ import com.intellij.execution.process.ProcessEvent
 import com.intellij.formatting.service.AsyncDocumentFormattingService
 import com.intellij.formatting.service.AsyncFormattingRequest
 import com.intellij.formatting.service.FormattingService
+import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import org.intellij.markdown.flavours.gfm.GFMFlavourDescriptor
+import org.intellij.markdown.html.HtmlGenerator
+import org.intellij.markdown.parser.MarkdownParser
 import org.vlang.configurations.VlangConfigurationUtil
 import org.vlang.configurations.VlangProjectSettingsState.Companion.projectSettings
 import org.vlang.lang.psi.VlangFile
@@ -55,7 +61,8 @@ class VlangFormattingService : AsyncDocumentFormattingService() {
                             if (exitCode == 0) {
                                 request.onTextReady(output.stdout)
                             } else {
-                                request.onError("Can't format", output.stderr)
+                                val (offset, error) = formatFmtError(request.context.psiElement, output.stderr)
+                                request.onError("Can't format", error, offset)
                             }
                         }
                     })
@@ -67,13 +74,48 @@ class VlangFormattingService : AsyncDocumentFormattingService() {
                     return true
                 }
 
-                override fun isRunUnderProgress(): Boolean {
-                    return true
-                }
+                override fun isRunUnderProgress(): Boolean = true
             }
         } catch (e: ExecutionException) {
             request.onError("Can't format", e.message ?: "")
             null
         }
+    }
+
+    private fun formatFmtError(context: PsiElement, error: String): Pair<Int, String> {
+        val errorIndex = error.indexOf("error:")
+        if (errorIndex == -1) {
+            return Pair(0, error)
+        }
+
+        val filenameAndLine = error.substring(0, errorIndex).trim()
+        val fileParts = filenameAndLine.split(":")
+        val line = fileParts.getOrElse(1) { "-1" }.toIntOrNull() ?: -1
+        val column = fileParts.getOrElse(2) { "0" }.toIntOrNull() ?: 0
+        val firstNewLineIndex = error.indexOf("\n")
+        val errorText = error.substring(errorIndex + 6, firstNewLineIndex)
+
+        val flavour = GFMFlavourDescriptor()
+        val root = MarkdownParser(flavour).buildMarkdownTreeFromString(errorText)
+        val errorTextHtml = HtmlGenerator(errorText, root, flavour)
+            .generateHtml()
+            .removePrefix("<body>")
+            .removeSuffix("</body>")
+
+        val file = context.containingFile
+        val document = PsiDocumentManager.getInstance(context.project).getDocument(file)
+        val lineOffset = document?.getLineStartOffset(line - 1) ?: -1
+        val offset = lineOffset + column
+
+        var internalVfmtIndex = error.indexOf("Internal vfmt")
+        if (internalVfmtIndex == -1) {
+            internalVfmtIndex = error.lastIndex
+        }
+
+        val codeBlock = error.substring(firstNewLineIndex, internalVfmtIndex).trimEnd()
+        val codeHtml = HTMLTextPainter.convertCodeFragmentToHTMLFragmentWithInlineStyles(context, codeBlock)
+        val errorHtml = errorTextHtml + codeHtml
+
+        return offset to errorHtml
     }
 }
