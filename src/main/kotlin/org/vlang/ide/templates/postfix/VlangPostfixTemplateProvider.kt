4a8901607c6f8877aa8c1e1b8c69d4ccfbe4adb5
i582 <51853996+i582@users.noreply.github.com>
2023-02-04 22:39:26 +0400

    added dump() postfix template

--- src/main/kotlin/org/vlang/ide/templates/postfix/VlangPostfixTemplateProvider.kt
+++ src/main/kotlin/org/vlang/ide/templates/postfix/VlangPostfixTemplateProvider.kt
@@ -14,6 +14,7 @@ class VlangPostfixTemplateProvider : PostfixTemplateProvider {
         VlangPrintlnPostfixTemplate(),
         VlangEprintPostfixTemplate(),
         VlangEprintlnPostfixTemplate(),
+        VlangDumpPostfixTemplate(),
         VlangParPostfixTemplate(),
         VlangReturnPostfixTemplate(),
         VlangMatchPostfixTemplate(),
