20435fbc895328b3df933c5822f108d08bfd2a4c
i582 <51853996+i582@users.noreply.github.com>
2023-02-06 00:04:43 +0400

    allow installing for 2023.1 EAPs

--- src/main/kotlin/org/vlang/project/MissingToolchainNotificationProvider.kt
+++ src/main/kotlin/org/vlang/project/MissingToolchainNotificationProvider.kt
@@ -4,11 +4,11 @@ import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.ui.EditorNotificationPanel
-import com.intellij.util.io.exists
 import org.vlang.toolchain.VlangToolchain
 import org.vlang.toolchain.VlangToolchainService.Companion.toolchainSettings
 import org.vlang.utils.isNotVlangFile
 import org.vlang.utils.toPath
+import java.nio.file.Files
 
 class MissingToolchainNotificationProvider(project: Project) : VlangEditorNotificationProviderBase(project) {
     override fun createNotificationPanel(file: VirtualFile, editor: FileEditor, project: Project): EditorNotificationPanel? {
@@ -22,7 +22,7 @@ class MissingToolchainNotificationProvider(project: Project) : VlangEditorNotifi
         }
 
         val homePath = toolchain.homePath()
-        if (!homePath.toPath().exists()) {
+        if (!Files.exists(homePath.toPath())) {
             return createToolchainNotification("V toolchain folder $homePath is not exist", project, file)
         }
 
