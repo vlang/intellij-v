6487a40e2f6c3c8f834293d3702a998bcc8bc122
i582 <51853996+i582@users.noreply.github.com>
2023-03-01 09:47:15 +0400

    added missing PSI files

---
+++ src/main/gen/org/vlang/lang/psi/impl/VlangShiftLeftExprImpl.java
@@ -0,0 +1,36 @@
+// This is a generated file. Not intended for manual editing.
+package org.vlang.lang.psi.impl;
+
+import java.util.List;
+import org.jetbrains.annotations.*;
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import org.vlang.lang.psi.VlangPsiTreeUtil;
+import static org.vlang.lang.VlangTypes.*;
+import org.vlang.lang.psi.*;
+
+public class VlangShiftLeftExprImpl extends VlangBinaryExprImpl implements VlangShiftLeftExpr {
+
+  public VlangShiftLeftExprImpl(@NotNull ASTNode node) {
+    super(node);
+  }
+
+  @Override
+  public void accept(@NotNull VlangVisitor visitor) {
+    visitor.visitShiftLeftExpr(this);
+  }
+
+  @Override
+  public void accept(@NotNull PsiElementVisitor visitor) {
+    if (visitor instanceof VlangVisitor) accept((VlangVisitor)visitor);
+    else super.accept(visitor);
+  }
+
+  @Override
+  @NotNull
+  public VlangShiftLeftOp getShiftLeftOp() {
+    return notNullChild(VlangPsiTreeUtil.getChildOfType(this, VlangShiftLeftOp.class));
+  }
+
+}
