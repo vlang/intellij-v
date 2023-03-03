1d62ed60da4c0715a58b45fba29404f139f22fba
i582 <51853996+i582@users.noreply.github.com>
2023-03-03 17:16:44 +0400

    fixed test file name

--- src/test/kotlin/org/vlang/ide/annotators/VlangOptionResultProblemsTest.kt
+++ src/test/kotlin/org/vlang/ide/annotators/VlangOptionResultProblemsTest.kt
@@ -3,6 +3,6 @@ package org.vlang.ide.annotators
 class VlangOptionResultProblemsTest : AnnotatorTestBase("result-option") {
     fun `test err always none`() = doTest("err_is_always_none.v")
     fun `test or expr for non result option`() = doTest("or_expr_for_non_result_option.v")
-    fun `test wrong usage of Exclamation Mark or Question Mark`() = doTest("wrong_usage_of_!_or_?.v")
+    fun `test wrong usage of Exclamation Mark or Question Mark`() = doTest("wrong_usage_of_excl_or_quest_mark.v")
     fun `test unwrapping for non result option`() = doTest("unwrapping_for_non_result_option.v")
 }
