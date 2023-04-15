9e0ca083872c89a3c0fa5edd22ad719de42b5a41
i582 <51853996+i582@users.noreply.github.com>
2023-04-15 18:02:04 +0400

    fixed naming inspection for `_` variable

--- src/test/resources/inspections/namingConventions/variables.v
+++ src/test/resources/inspections/namingConventions/variables.v
@@ -1,4 +1,9 @@
 module main
+
 fn main() {
     <error descr="Variable name cannot contain uppercase letters, use snake_case instead">InvalidVarName</error> := 1
+
+   <error descr="Variable name cannot start with '_'">_with_</error> := 100 // error
+   snake_case := 100 // ok
+   _ := 100 // ok
 }
