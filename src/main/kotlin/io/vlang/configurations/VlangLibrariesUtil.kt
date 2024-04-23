d90aaf9e2e07166642ef792fd0664f8c4eaf7d8e
Jakub Jabłoński <jakub.jablonski@outfittery.de>
2024-04-23 17:18:13 +0200

    fix "java.lang.Throwable: Write-unsafe context!" while updating libraries

--- src/main/kotlin/io/vlang/configurations/VlangLibrariesUtil.kt
+++ src/main/kotlin/io/vlang/configurations/VlangLibrariesUtil.kt
@@ -1,5 +1,6 @@
 package io.vlang.configurations
 
+import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.application.WriteAction
 import com.intellij.openapi.application.invokeLater
 import com.intellij.openapi.project.Project
@@ -8,7 +9,7 @@ import com.intellij.openapi.roots.ex.ProjectRootManagerEx
 
 object VlangLibrariesUtil {
     fun updateLibraries(project: Project) {
-        invokeLater {
+        invokeLater(ModalityState.nonModal()) {
             WriteAction.run<RuntimeException> {
                 ProjectRootManagerEx.getInstanceEx(project).makeRootsChange({}, RootsChangeRescanningInfo.TOTAL_RESCAN)
             }
