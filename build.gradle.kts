becf91c590112abe10370a0b88a6a947865908bf
Jakub Jabłoński <jakub.jablonski@outfittery.de>
2025-05-27 16:26:29 +0200

    Apparently untilBuild needs to be null to remove it

--- build.gradle.kts
+++ build.gradle.kts
@@ -128,6 +128,7 @@ intellijPlatform {
         ideaVersion {
             sinceBuild = properties("pluginSinceBuild")
 //            untilBuild = properties("pluginUntilBuild")
+            untilBuild = provider { null }
         }
     }
 
